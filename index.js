9da05b6011fb4dda4fd81c5db688a27071382ba3
Nick Cooper <nickcooper84@gmail.com>
2019-04-15 20:02:07 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase()
+    console.log('I can\'t hear you!')
+  }
+function sayHiToGrandma(string) {
+  return string.toUpperCase()
+    console.log('YES INDEED!')
+  }
+function sayHiToGrandma('I love you, grandma.') {
+  console.log('I love you, too.')
+}
